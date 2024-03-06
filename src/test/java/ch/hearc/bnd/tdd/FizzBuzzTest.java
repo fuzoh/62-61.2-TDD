362e8c4fc6aad99c42891b4ce2d0fd56a932ffb8
fuzoh <info@bastiennicoud.ch>
2024-03-06 09:07:09 +0100

    test: FizzBuzz base test cases

--- src/test/java/ch/hearc/bnd/tdd/FizzBuzzTest.java
+++ src/test/java/ch/hearc/bnd/tdd/FizzBuzzTest.java
@@ -1,5 +1,7 @@
 package ch.hearc.bnd.tdd;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,5 +13,35 @@ class FizzBuzzTest {
     @ValueSource(ints = {5,10,20,25,45,50,100,125})
     void FizzForMultipleOfFive(int number) {
         assertEquals("Fizz", FizzBuzz.fizzBuzz(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {7,14,28,56,77,91})
+    void BuzzForMultipleOfSeven(int number) {
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {35,70,140})
+    void FizzBuzzForMultipleOfFiveAndSeven(int number) {
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,4,8,11,13,16,17,19,22,23,26,29,31,32,34,37,38,41,43,46,47,49,52,53,58,59,61,62,64,67,68,71,73,74,76,79,82,83,86,89,92,94,97,98})
+    void NumberForOtherCases(int number) {
+        assertEquals(String.valueOf(number), FizzBuzz.fizzBuzz(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -2, -5, -7, -10, -14, -35, -70, -140})
+    void ExceptionForNegativeOrZero(int number) {
+        assertThrows(IllegalArgumentException.class, () -> FizzBuzz.fizzBuzz(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {66,666,6666})
+    void FizzBuzzFor66Number(int number) {
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(number));
     }
 }
